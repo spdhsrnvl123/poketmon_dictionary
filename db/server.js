5c1e78938249e9676972ab756a7665a8f8ffefce
이태형 <spdhsrnvl456@gmail.com>
2023-02-18 22:08:13 +0900

    initial commit

--- db/server.js
+++ db/server.js
@@ -11,10 +11,11 @@ const port = process.env.PORT || 3001;
 
 server.use(middlewares);
 
-server.use(jsonServer.bodyParser);
+server.use(jsonServer.rewriter({
+    "/api/*":"/$1",
+}));
 
 server.use(router);
-
 server.listen(port, () => {
     console.log('JSON Server is running');
 });
