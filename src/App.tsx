09ca4f32f54b0a1ccc8100ff8a6e8fd46be4942d
leutbing <spdhsrnvl456@gmail.com>
2025-02-23 21:24:55 +0900

    feat:업 버튼 구현 및 포켓몬 디테일 페이지 스타일링

--- src/App.tsx
+++ src/App.tsx
@@ -5,39 +5,9 @@ import GlobalStyle from "./assets/styles/GlobalStyle";
 import Router from "./router/router";
 import Header from "./layouts/Header";
 import AddButton from "./components/AddButton";
-import UpButton from "./components/UpButton";
 import Category from "./layouts/Category";
 
-const Footer = styled.footer`
-  min-width: 100%;
-  background-color: #ffffff;
-  position: absolute;
-  bottom: 0;
-  z-index: 2; //footer 문제로 z-index 추가.
-  padding-top:10px;
-  padding-bottom: 10px;
-
-  .inner_footer {
-    display: flex;
-    justify-content: space-between;
-    margin: 0 5px;
-    small {
-      font-size: 12px;
-      line-height: 21px;
-      color: #999;
-    }
-    .area_link {
-      span {
-        padding-left: 10px;
-        font-size: 14px;
-        color: #999;
-      }
-    }
-  }
-`;
-
 function App() {
-
   return (
     <>
       <GlobalStyle />
@@ -45,17 +15,8 @@ function App() {
       <SearchContainer />
       <Category />
       <Router />
+
       <AddButton />
-      {/* <Footer>
-        <div className="inner_footer">
-          <small>&copy; poketmon dictionary</small>
-          <div className="area_link">
-            <span>beverage</span>
-            <span>goods</span>
-            <span>store</span>
-          </div>
-        </div>
-      </Footer> */}
     </>
   );
 }
